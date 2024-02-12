8f500f0f5d6423cebcf12fae8a3dcd7fcc10ac60
Royal <royalhuseyn902@gmail.com>
2024-02-12 18:18:22 +0400

    Create empty array with 16 linked lists when instatiating a hash map

--- hash-map.js
+++ hash-map.js
@@ -1,3 +1,10 @@
-import LinkedList from './linked-list';
+import LinkedList from './linked-list.js';
 
-class HashMap {}
+class HashMap {
+  constructor() {
+    this.buckets = new Array(16).fill(new LinkedList());
+  }
+}
+
+const hm = new HashMap();
+console.log(hm);
